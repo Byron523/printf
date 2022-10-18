5b6534db88fcfb20f70473898011b84dbd2d9477
lynlisa23 <lynlisadondo@gmail.com>
2022-10-18 22:52:05 +0200

    our long functions

---
+++ long_func.c
@@ -0,0 +1,155 @@
+#include "main.h"
+
+/**
+ * long_dec_hex - prints a long decimal in hexadecimal
+ * @ptr: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+int long_dec_hex(va_list ptr, char *buf, unsigned int ibuf)
+{
+	long int int_input, i, isnegative, count, first_digit;
+	char *hexadecimal, *binary;
+
+	int_input = va_arg(ptr, long int);
+	isnegative = 0;
+	if (int_input == 0)
+	{
+		ibuf = handl_buf(buf, '0', ibuf);
+		return (1);
+	}
+	if (int_input < 0)
+	{
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+
+	binary = malloc(sizeof(char) * (64 + 1));
+	binary = _binary_array(binary, int_input, isnegative, 64);
+	hexadecimal = malloc(sizeof(char) * (16 + 1));
+	hexadecimal = _hex_array(binary, hexadecimal, 0, 16);
+	for (first_digit = i = count = 0; hexadecimal[i]; i++)
+	{
+		if (hexadecimal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = handl_buf(buf, hexadecimal[i], ibuf);
+			count++;
+		}
+	}
+	free(binary);
+	free(hexadecimal);
+	return (count);
+}
+
+/**
+ * _long_int - prints a long integer
+ * @ptr: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+int _long_int(va_list ptr, char *buf, unsigned int ibuf)
+{
+	long int int_input;
+	unsigned long int int_in, int_temp, i, div, isneg;
+
+	int_input = va_arg(ptr, long int);
+	isneg = 0;
+	if (int_input < 0)
+	{
+		int_in = int_input * -1;
+		ibuf = handl_buf(buf, '-', ibuf);
+		isneg = 1;
+	}
+	else
+	{
+		int_in = int_input;
+	}
+
+	int_temp = int_in;
+	div = 1;
+	while (int_temp > 9)
+	{
+		div *= 10;
+		int_temp /= 10;
+	}
+	for (i = 0; div > 0; div /= 10, i++)
+	{
+		ibuf = handl_buf(buf, ((int_in / div) % 10) + '0', ibuf);
+	}
+	return (i + isneg);
+}
+
+/**
+ * _long_oct - prints long decimal number in octal
+ * @ptr: input number
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+int _long_oct(va_list ptr, char *buf, unsigned int ibuf)
+{
+	long int int_input, i, isnegative, count, first_digit;
+	char *octal, *binary;
+
+	int_input = va_arg(ptr, long int);
+	isnegative = 0;
+	if (int_input == 0)
+	{
+		ibuf = handl_buf(buf, '0', ibuf);
+		return (1);
+	}
+	if (int_input < 0)
+	{
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+
+	binary = malloc(sizeof(char) * (64 + 1));
+	binary = _binary_array(binary, int_input, isnegative, 64);
+	octal = malloc(sizeof(char) * (22 + 1));
+	octal = _long_oct_array(binary, octal);
+	for (first_digit = i = count = 0; octal[i]; i++)
+	{
+		if (octal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = handl_buf(buf, octal[i], ibuf);
+			count++;
+		}
+	}
+	free(binary);
+	free(octal);
+	return (count);
+}
+
+/**
+ - prints a long unsigned integer
+ * @ptr: number to print
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+int _unsigned_int(va_list ptr, char *buf, unsigned int ibuf)
+{
+	unsigned long int int_in, int_temp, i, div;
+
+	int_in = va_arg(ptr, unsigned long int);
+	int_temp = int_in;
+	div = 1;
+	while (int_temp > 9)
+	{
+		div *= 10;
+		int_temp /= 10;
+	}
+	for (i = 0; div > 0; div /= 10, i++)
+	{
+		ibuf = handl_buf(buf, ((int_in / div) % 10) + '0', ibuf);
+	}
+	return (i);
+}
+
